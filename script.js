b19dd9bba73fe028fe576106201cbbf9ef22d6cf
helenabodker <helena@boedker.dk>
2023-05-31 15:11:08 +0200

    Searching for Herning when loading

--- script.js
+++ script.js
@@ -59,7 +59,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -149,3 +148,4 @@ let locationButton = document.querySelector("#locationButton");
 locationButton.addEventListener("click", getPosition);
 
 //Start
+searchCity("Herning");
